f0caaff8410f8d0e01f23625f8bb076ec188077d
Vass Zsolt <rtmzsolt@gmail.com>
2021-09-07 16:14:48 +0200

    One possible way to initial the starting position

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -23,10 +23,42 @@ namespace Chess
         private void Form1_Load(object sender, EventArgs e)
         {
             gameLogicManager = new GameLogicManager();
-            ChessPiece[,] x =gameLogicManager.Initialize();
-            
-            
-        }
+            ChessPiece[,] x = gameLogicManager.Initialize();
+            int piece_type, piece_color;
+            foreach (Button button in Panel_Board.Controls)
+            {
+                int column = button.Name[0] - 65;
+                int row = (int)char.GetNumericValue(button.Name[1]) - 1;
 
+            }
+
+        }
     }
 }
+
+/*
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        if (x[i, j] is not null)
+        {
+            piece_type = (int)x[i, j].Type;
+            piece_color = (int)x[i, j].Color;
+            if (piece_color==0)
+            {
+                switch ((piece_type))
+                {
+                    case 0:
+                        {
+                            button.BackgroundImage = Image.FromFile(GetPath("b_pawn.png"));
+                        }
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+*/
