99afa0e50cc1320838cb683815e039a22a7c07dc
VassZsolt <rtmzsolt@gmail.com>
2021-10-05 11:25:22 +0200

    Comment fixed (at Rook moves)

--- GameLogic/GameLogicManager.cs
+++ GameLogic/GameLogicManager.cs
@@ -138,7 +138,7 @@ namespace GameLogic
                         {
                             if (board[t_row, t_column] is null)
                             {
-                                if (board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 1 && f_column == t_column                    //Black Pawn move 1 up
+                                if (board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 1 && f_column == t_column                   //Black Pawn move 1 up
                                 || board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 2 && f_column == t_column && f_row == 1      //Black Pawn move 2 up
                                 || board[f_row, f_column].Color == PieceColor.White && f_row == t_row + 1 && f_column == t_column                    //White Pawn move 1 down
                                 || board[f_row, f_column].Color == PieceColor.White && f_row == t_row + 2 && f_column == t_column && f_row == 6)     //White Pawn move 2 down
@@ -150,8 +150,8 @@ namespace GameLogic
                             {
 
                                 if (board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 1 && f_column == t_column + 1                //Black Pawn hit left         
-                                || board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 1 && f_column == t_column - 1                //Black Pawn hit right
-                                || board[f_row, f_column].Color == PieceColor.White && f_row == t_row + 1 && f_column == t_column + 1                //White Pawn hit left
+                                || board[f_row, f_column].Color == PieceColor.Black && f_row == t_row - 1 && f_column == t_column - 1                 //Black Pawn hit right
+                                || board[f_row, f_column].Color == PieceColor.White && f_row == t_row + 1 && f_column == t_column + 1                 //White Pawn hit left
                                 || board[f_row, f_column].Color == PieceColor.White && f_row == t_row + 1 && f_column == t_column - 1)                //White Pawn hit right
                                 {
                                     Move(f_row, f_column, t_row, t_column);
@@ -256,7 +256,7 @@ namespace GameLogic
                                     i++;
                                 } while (i < t_row);
                             }
-                            if (f_row == t_row && f_column > t_column) //Moving Right
+                            if (f_row == t_row && f_column > t_column) //Moving Left
                             {
                                 do
                                 {
